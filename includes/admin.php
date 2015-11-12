39986b4afd16979ca7e9300a23a691ad86068151
Lars Jensen <lars.jensen@exenova.dk>
2015-11-12 22:38:02 +0100

    Added comments to code.

--- includes/admin.php
+++ includes/admin.php
@@ -11,28 +11,28 @@ class WPTagSanitizerAdmin extends WPTagSanitizer {
             wp_die(__('You do not have sufficient permissions to access this page.'));
         }
 
-        if (isset($_POST["WPTagSanitizer_JSONTABLE"])) {
-            $textAreaValue = stripslashes($_POST["WPTagSanitizer_JSONTABLE"]);
+        if (isset($_POST[self::$plugin_name."_JSONTABLE"])) {
+            $textAreaValue = stripslashes($_POST[self::$plugin_name."_JSONTABLE"]);
             $transformTable = self::getTransformTable($textAreaValue);
 
             if ($transformTable != false) {
-                update_option("WPTagSanitizer_JSONTABLE", $textAreaValue);
-                update_option("WPTagSanitizer_TRANSFORMTABLE", $transformTable);
+                update_option(self::$plugin_name."_JSONTABLE", $textAreaValue);
+                update_option(self::$plugin_name."_TRANSFORMTABLE", $transformTable);
             }
         } else {
-            $textAreaValue = get_option("WPTagSanitizer_JSONTABLE");
+            $textAreaValue = get_option(self::$plugin_name."_JSONTABLE");
         }
 
         // debug
         if (self::$debug) {
             echo '<pre>';
-            echo 'get_option("WPTagSanitizer_MESSAGES")=' . print_r(get_option("WPTagSanitizer_MESSAGES")) . PHP_EOL;
-            echo 'get_option("WPTagSanitizer_JSONTABLE")=' . print_r(get_option("WPTagSanitizer_JSONTABLE")) . PHP_EOL;
-            echo 'get_option("WPTagSanitizer_TRANSFORMTABLE")=' . print_r(get_option("WPTagSanitizer_TRANSFORMTABLE")) . PHP_EOL;
+            echo 'get_option(self::$plugin_name."_MESSAGES")=' . print_r(get_option(self::$plugin_name."_MESSAGES")) . PHP_EOL;
+            echo 'get_option(self::$plugin_name."_JSONTABLE")=' . print_r(get_option(self::$plugin_name."_JSONTABLE")) . PHP_EOL;
+            echo 'get_option(self::$plugin_name."_TRANSFORMTABLE")=' . print_r(get_option(self::$plugin_name."_TRANSFORMTABLE")) . PHP_EOL;
             echo '</pre>';
         }
 
-        $messages = get_option("WPTagSanitizer_MESSAGES");
+        $messages = get_option(self::$plugin_name."_MESSAGES");
 
         while (!empty($messages)) {
             $message = array_shift($messages);
@@ -40,14 +40,14 @@ class WPTagSanitizerAdmin extends WPTagSanitizer {
         }
 
         // since the messages has been shown, purge them.
-        update_option("WPTagSanitizer_MESSAGES", []);
+        update_option(self::$plugin_name."_MESSAGES", []);
 
         // print the admin page
         echo '<div class="wrap">';
         echo '<h2>'.self::$plugin_name.'</h2>';
         echo '<p>New tags will be normalized according to these rules. Normalization is case insensitive, however the resulting tag is not.';
         echo '<form method="post" action="">';
-        echo '<textarea name="WPTagSanitizer_JSONTABLE" style="width: 50%;height: 400px;">' . $textAreaValue . '</textarea>';
+        echo '<textarea name="'.self::$plugin_name.'_JSONTABLE" style="width: 50%;height: 400px;">' . $textAreaValue . '</textarea>';
         echo '<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>';
         echo '</form></div>';
     }
